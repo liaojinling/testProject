50c94ca64d05158e6b3b7c03bed7218f7cea10b0
liaojinling <18606990406@163.com>
2017-08-24 16:22:03 +0800

    删除文件夹Ltapitest

--- Ltapitest/src/main/java/com/api/constant/ConfigConstValue.java
+++
@@ -1,9 +0,0 @@
-package com.api.constant;
-
-/**
- * Created by owen on 2017-7-13.
- */
-public class ConfigConstValue {
-    public static int dubbotimeout = 0;
-    public static String dubboAddress = "";
-}
